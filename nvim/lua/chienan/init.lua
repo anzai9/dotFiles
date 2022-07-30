ae53b331a00cbed833d10355014c8756389247ce
Chien-An Wang <jiananwang1209@gmail.com>
2022-07-31 03:00:02 +0800

    feat(nvim): add impatient to speed up loading startup time

--- nvim/lua/chienan/init.lua
+++ nvim/lua/chienan/init.lua
@@ -1,5 +1,6 @@
 -- packer plugins
 require("chienan.plugins")
+require("impatient").enable_profile()
 -- some utils
 require("chienan.utils")
 -- plugins config
--- nvim/lua/chienan/plugins.lua
+++ nvim/lua/chienan/plugins.lua
@@ -11,6 +11,8 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   
   use {'neoclide/coc.nvim', branch = 'release'}
+
+  use {'lewis6991/impatient.nvim'}
   
   
   -- Telescope
