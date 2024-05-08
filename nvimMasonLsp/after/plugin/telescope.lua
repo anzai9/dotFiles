b618606d0417872563444a3ad55534ae8bc1fb7d
Chien-An Wang <jiananwang1209@gmail.com>
2024-05-08 10:55:08 +0800

    chore: format change

--- nvimMasonLsp/after/plugin/telescope.lua
+++ nvimMasonLsp/after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local actions = require('telescope.actions')
 local action_layout = require('telescope.actions.layout')
 local action_state = require("telescope.actions.state")
 
-local keymapOpts = { noremap = true, silent = true }
+local keymapOpts = {noremap = true, silent = true}
 vim.keymap.set('n', '<leader>ff', builtin.find_files, keymapOpts)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, keymapOpts)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, keymapOpts)
@@ -14,7 +14,8 @@ vim.keymap.set('n', '<leader>*', builtin.grep_string, keymapOpts)
 vim.keymap.set('n', '<leader>:', builtin.command_history, keymapOpts)
 vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, keymapOpts)
 vim.keymap.set('n', '<leader>fT', '<cmd>TodoTelescope<cr>', keymapOpts)
-vim.keymap.set('n', '<leader>ft', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', keymapOpts)
+vim.keymap.set('n', '<leader>ft',
+               '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', keymapOpts)
 vim.keymap.set('n', '<leader>fa', builtin.autocommands, keymapOpts)
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, keymapOpts)
 
@@ -23,23 +24,18 @@ local custom_actions = {}
 custom_actions.project_files = function()
     local opts = {}
     local ok = pcall(builtin.git_files, opts)
-    if not ok then
-        builtin.find_files(opts)
-    end
+    if not ok then builtin.find_files(opts) end
 end
 
 -- fallback to find_files while the git_files cannot find a .git directory
-vim.keymap.set('n', '<C-p>', function()
-    custom_actions.project_files()
-end, { silent = true })
+vim.keymap.set('n', '<C-p>', function() custom_actions.project_files() end,
+               {silent = true})
 
 -- open multiple files at once
 custom_actions._multiple_open = function(prompt_bufnr, open_cmd)
     local picker = action_state.get_current_picker(prompt_bufnr)
     local search_res_count = picker.manager:num_results()
-    if search_res_count == 0 then
-        return
-    end
+    if search_res_count == 0 then return end
     local selected_count = #picker:get_multi_selection()
     if not selected_count or selected_count <= 1 then
         actions.add_selection(prompt_bufnr)
@@ -48,17 +44,20 @@ custom_actions._multiple_open = function(prompt_bufnr, open_cmd)
     vim.cmd("cfdo " .. open_cmd)
 end
 
-custom_actions.multi_selection_open_vsplit = function(prompt_bufnr)
-    custom_actions._multiple_open(prompt_bufnr, "vsplit")
-end
+custom_actions.multi_selection_open_vsplit =
+    function(prompt_bufnr)
+        custom_actions._multiple_open(prompt_bufnr, "vsplit")
+    end
 
-custom_actions.multi_selection_open_split = function(prompt_bufnr)
-    custom_actions._multiple_open(prompt_bufnr, "split")
-end
+custom_actions.multi_selection_open_split =
+    function(prompt_bufnr)
+        custom_actions._multiple_open(prompt_bufnr, "split")
+    end
 
-custom_actions.multi_selection_open_tab = function(prompt_bufnr)
-    custom_actions._multiple_open(prompt_bufnr, "tabe")
-end
+custom_actions.multi_selection_open_tab =
+    function(prompt_bufnr)
+        custom_actions._multiple_open(prompt_bufnr, "tabe")
+    end
 
 custom_actions.multi_selection_open = function(prompt_bufnr)
     custom_actions._multiple_open(prompt_bufnr, "edit")
@@ -69,18 +68,10 @@ require("telescope").load_extension("harpoon")
 require("telescope").load_extension("git_worktree")
 require('telescope').setup {
     defaults = {
-        path_display = {
-            shorten = { len = 3, exclude = { 1, -1 } }
-        },
+        path_display = {shorten = {len = 5, exclude = {1, -1}}},
         vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--trim",
+            "rg", "--color=never", "--no-heading", "--with-filename",
+            "--line-number", "--column", "--smart-case", "--trim"
         },
         mappings = {
             i = {
@@ -92,38 +83,30 @@ require('telescope').setup {
                 ["<CR>"] = custom_actions.multi_selection_open,
                 ["<C-V>"] = custom_actions.multi_selection_open_vsplit,
                 ["<C-S>"] = custom_actions.multi_selection_open_split,
-                ["<C-T>"] = custom_actions.multi_selection_open_tab,
+                ["<C-T>"] = custom_actions.multi_selection_open_tab
             }
         }
     },
     pickers = {
         buffers = {
             previewer = false,
-            mappings = {
-                i = {
-                    ["<C-b>"] = "delete_buffer",
-                }
-            }
+            mappings = {i = {["<C-b>"] = "delete_buffer"}}
         },
         find_files = {
             previewer = false,
-            find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+            find_command = {"fd", "--type", "f", "--strip-cwd-prefix"}
         },
-        oldfiles = {
-            previewer = false,
-        },
-        git_files = {
-            previewer = false,
-        }
+        oldfiles = {previewer = false},
+        git_files = {previewer = false}
     },
     extensions = {
         fzf = {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
-            case_mode = "smart_case",
+            case_mode = "smart_case"
         }
-    },
+    }
 }
 
 vim.keymap.set("n", "<leader>gw", function()
--- nvimMasonLsp/after/plugin/treesitter.lua
+++ nvimMasonLsp/after/plugin/treesitter.lua
@@ -1,50 +1,20 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = {
-    "python",
-    "go",
-    "lua",
-    "c",
-    "cpp",
-    "vim",
-    "javascript",
-    "typescript",
-    "tsx",
-    "html",
-    "css",
-    "yaml",
-    "toml",
-    "json",
-    "rust",
-    "markdown",
-    "markdown_inline",
-    "graphql",
-    "bash",
-    "regex",
-    "jsdoc",
-  },
-  sync_install = false,
+    ensure_installed = {
+        "python", "go", "lua", "c", "cpp", "vim", "javascript", "typescript",
+        "tsx", "html", "css", "yaml", "toml", "json", "rust", "markdown",
+        "markdown_inline", "graphql", "bash", "regex", "jsdoc"
+    },
+    sync_install = false,
 
-  auto_install = true,
+    auto_install = true,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {enable = true, additional_vim_regex_highlighting = false},
 
-  indent = {
-    enable = true,
-    disable = {},
-  },
+    indent = {enable = true, disable = {}},
 
-  incremental_selection = {
-    enabble = true,
-  },
+    incremental_selection = {enabble = true},
 
-  textObjects = {
-    enable = true,
-  },
+    textObjects = {enable = true},
 
-  autotag = {
-    enable = true,
-  }
+    autotag = {enable = true}
 }
