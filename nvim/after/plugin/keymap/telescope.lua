015a24a2486581949f87cc220f2aab29fb315ecb
Chien-An Wang <jiananwang1209@gmail.com>
2024-05-10 02:06:07 +0800

    feat(nvim): migrate to lazy package manager and retire ori nvim

--- nvim/after/plugin/keymap/telescope.lua
+++
@@ -1,9 +0,0 @@
-local nnoremap = require("chienan.utils").nnoremap
-
-nnoremap("<leader>gw", function()
-  require("telescope").extensions.git_worktree.git_worktrees()
-end)
-
-nnoremap("<leader>gm", function()
-  require("telescope").extensions.git_worktree.create_git_worktree()
-end)
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,4 @@
-require("chienan")
-
-local core_conf_files = {
-  "autocommands.vim",
-  "plugins.vim",
-  "set.vim"
-}
-
-for _idx, fname in ipairs(core_conf_files) do
-  local conf_path = vim.fn.stdpath("config") .. "/core/" .. fname
-  vim.cmd("source " .. conf_path)
-end
+require("config.set")
+require("config.remap")
+require("config.lazy")
+require("config.autocmds")
